c121b2d9a9070ffb48b88299b9c5bc8242aa356e
Manuel Diaz <Mana.diaz@duocuc.cl>
2021-12-15 15:24:33 -0300

    main

--- e2e/src/test2.e2e-spec.ts
+++ e2e/src/test2.e2e-spec.ts
@@ -1,6 +1,6 @@
-import { AppPage } from './app.po';
 import { browser, element, by } from "protractor";
-describe('Nombre de la app en Recuperar Password', () => {
+
+describe('Pruebas e2e', () => {
 
     beforeEach(() => {
         browser.get('/p-recuperar-password');
@@ -9,5 +9,14 @@ describe('Nombre de la app en Recuperar Password', () => {
     it('Muestra nombre de la App', () => {
         expect(element(by.css('.page-selected ion-title')).getText()).toContain('LlevoAPP');
     });
+
+    it('Recuperar Password posee titulo de App', () => {
+        expect(element(by.css('.page-selected h2')).getText()).toContain('LLevoApp');
+    });
+
+    it('En el menu usuario aparece btn SALIR', () => {
+        expect(element(by.css('.page-selected ion-label')).getText()).toContain('Salir');
+    });
+
   
   });
